f456fb812bfd5ccd8bb356c75006e99cfdaba431
shibam17 <shibam.dipu@gmail.com>
2021-06-17 16:27:45 +0530

    added

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -7,6 +7,21 @@ function Detail() {
       <Background>
         <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg' />
       </Background>
+      <ImageTitle>
+        <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78' />
+      </ImageTitle>
+      <Controls>
+        <PlayButton>
+          <img src='/images/play-icon-black.png' />
+          <span>PLAY</span>
+        </PlayButton>
+        <TrailerButton>
+          <img src='/images/play-icon-white.png' />
+          <span>Trailer</span>
+        </TrailerButton>
+        <AddButton></AddButton>
+        <GroupWatchBUtton></GroupWatchBUtton>
+      </Controls>
     </Container>
   );
 }
@@ -20,15 +35,53 @@ const Container = styled.div`
 `;
 
 const Background = styled.div`
-    position: fixed;
-    top:0;
-    left:0;
-    bottom:0;
-    right:0;
-    z-index: -1;
-    img{
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+  opacity: 0.8;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `;
+
+const ImageTitle = styled.div`
+  height: 30vh;
+  width: 35vw;
+  min-height: 170px;
+  min-width: 200px;
+
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: contain;
+  }
+`;
+
+const Controls = styled.div`
+    display: flex;
+
+`;
+
+const PlayButton = styled.button`
+  border-radius: 4px;
+  font-size: 15px;
+  padding: 0 24px;
+  margin-right: 22px;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  background: rgb(249, 249, 249);
+  border: none;
+  letter-spacing: 1.8px;
+`;
+
+const TrailerButton = styled.button``;
+
+const AddButton = styled.button``;
+
+const GroupWatchBUtton = styled.button``;

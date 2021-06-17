efe3da844892e60ea0cb9ecd060de3d25f997a7c
shibam17 <shibam.dipu@gmail.com>
2021-06-17 16:13:14 +0530

    added

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 function Detail() {
-    return (
-        <div>
-            detail
-        </div>
-    )
+  return (
+    <Container>
+      <Background>
+        <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg' />
+      </Background>
+    </Container>
+  );
 }
 
-export default Detail
+export default Detail;
+
+const Container = styled.div`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+`;
+
+const Background = styled.div`
+    position: fixed;
+    top:0;
+    left:0;
+    bottom:0;
+    right:0;
+    z-index: -1;
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+`;

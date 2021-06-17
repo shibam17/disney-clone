b7e57a3a216d8399a05b02c30f5f664321cdc9c6
shibam17 <shibam.dipu@gmail.com>
2021-06-17 10:02:56 +0530

    adding viewers

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import ImageSlider from "./ImageSlider";
+import Viewers from "./Viewers";
 
 function Home() {
   return (
     <Container>
       <ImageSlider />
+      <Viewers />
     </Container>
   );
 }
@@ -16,6 +18,7 @@ const Container = styled.main`
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
   position: relative;
+  overflow-x: hidden;
   &:before {
     background: url("/images/home-background.png") center center / cover
       no-repeat fixed;

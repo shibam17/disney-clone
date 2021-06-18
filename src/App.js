28c5119b0d49a5f2bac4e2305cec804d9d435078
shibam17 <shibam.dipu@gmail.com>
2021-06-18 19:53:31 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Detail from "./components/Detail";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
       <Router>
         <Header />
         <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
           <Route path='/detail'>
             <Detail />
           </Route>
